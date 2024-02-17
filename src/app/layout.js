c95ab2072a16e90bf35d1adba062c0e4c145a756
StevanusAndika <stevanus0602@gmail.com>
2024-02-17 13:55:50 +0700

    upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import { Gabarito } from 'next/font/google'
 const gabarito = Gabarito({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'CuyAnimeList',
+  title: 'AnimeList',
   description: 'Website Anime Indonesia',
 }
 
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,5 @@
 "use client"
-import React from "react";
+
 
 import { FileSearch } from "@phosphor-icons/react/dist/ssr";
 
