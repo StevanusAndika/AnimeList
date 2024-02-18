db67208b9c89c8ff59b1a5a7eb307b4872aa3eb7
StevanusAndika <stevanus0602@gmail.com>
2024-02-19 02:59:33 +0700

    update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
 import Navbar from '@/components/Navbar'
 
 import { Gabarito } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
 
 const gabarito = Gabarito({ subsets: ['latin'] })
 
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${gabarito.className} bg-color-dark`} suppressHydrationWarning={true}>
         <Navbar />
+        <Analytics />
         {children}
       </body>
     </html>
