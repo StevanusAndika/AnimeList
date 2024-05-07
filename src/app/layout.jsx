daf6517526514db75fe9f6446262b62c7f29a4c5
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2024-05-08 02:03:22 +0700

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,12 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5648536853192084"
-          crossorigin="anonymous"
-        ></script>
-        <meta name="google-adsense-account" content="ca-pub-5648536853192084" />
+        
         {/* Tambahkan tag meta lainnya di sini jika perlu */}
       </Head>
       {/* Konten lainnya */}
